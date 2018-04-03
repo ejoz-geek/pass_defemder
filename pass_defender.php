0b510df19b010355f8ca59f04e8c65d20b348068
EJOZ <ejoz.geek@gmail.com>
2018-04-03 17:18:41 +0300

    Update pass_defender.php

--- pass_defender.php
+++ pass_defender.php
@@ -7,145 +7,182 @@
 ##                             ##
 @###############################@
 
-Version: 0.1
-Stage: pre-alpha
-Author: EJOZ
-Copyright © 2018 Stas Smirnov. All rights reserved.
+Copyright 2018 Stas Smirnov
 
-This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 
 */
 error_reporting(E_ALL);
-if ($argv[1] == 'gen_array')
+define('VER', '0.2 pre-alpha' . PHP_EOL);
+
+start:
+$command = readline('>');
+switch ($command)
 {
-	unset($argv);
-
-	define('CHARS', array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','r','s','t','u','v','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','R','S','T','U','V','X','Y','Z','0','1','2','3','4','5','6','7','8','9','.',',','(',')','[',']','!','?','&','^','%','@','*','$','<','>','|','+','-','{','}','`','~'));
-	define('LEN', 4096);
-
-	for ($i = 0; $i < LEN; $i++)
+	case 'help':
+		print_r('HELP' . PHP_EOL);
+		goto start;
+		break;
+	case 'ver':
+		print_r(VER);
+		goto start;
+		break;
+	case 'sha1':
+		print_r('SHA1: ' . sha1_file($_SERVER['PHP_SELF']) . PHP_EOL);
+		goto start;
+		break;
+	case 'md5':
+		print_r('MD5: ' . md5_file($_SERVER['PHP_SELF']) . PHP_EOL);
+		goto start;
+		break;
+	case 'gen_array':
+		gen_array();
+		goto start;
+		break;
+	case 'add_record':
+		add_record();
+		goto start;
+		break;
+	case 'decrypt':
+		decrypt();
+		goto start;
+		break;
+	case 'exit':
+		exit('Exit. Called by user...');
+		break;
+	default:
+		print_r('Unknown command!' . PHP_EOL);
+		goto start;
+		break;
+}
+function gen_array ()
+{
+	gen_array_start:
+	print_r('Type array length...' . PHP_EOL);
+	$len = readline('>');
+	if (!is_int($len) && ($len < 1000 || $len > 100000))
 	{
-		$index = random_int(0, count(CHARS) - 1);
-		@$array .= CHARS[$index];
-	}
-	unset($index);
-	unset($i);
-
-	$f = fopen(dirname($_SERVER['PHP_SELF']) . '/array.dat', 'w+b');
-	fwrite($f, base64_encode($array));
-	unset($array);
-	fclose($f);
-	unset($f);
-
-	$f = fopen(dirname($_SERVER['PHP_SELF']) . '/array.dat', 'rb');
-	$array = base64_decode(fread($f, filesize(dirname($_SERVER['PHP_SELF']) . '/array.dat')));
-	fclose($f);
-	unset($f);
-	if (strlen($array) != LEN)
-	{
-		print_r('ERROR! PLEASE RETRY.');
-		unset($array);
-		exit(1);
-	}
-	elseif (strlen($array) == LEN)
-	{
-		print_r('SUCCESS!');
-		unset($array);
-		exit(0);
+		print_r('Invalid value. The length must be a number. Not less than 1000 and not more than 100000' . PHP_EOL);
+		goto gen_array_start;
 	}
 	else
 	{
-		print_r('UNKNOWN ERROR!');
-		unset($array);
-		exit(5);
-	}
-}
-elseif ($argv[1] == 'add_entry')
-{	
-	$name = $argv[2];
-	$login = $argv[3];
-	unset($argv);
+		define('CHARS', array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','r','s','t','u','v','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','R','S','T','U','V','X','Y','Z','0','1','2','3','4','5','6','7','8','9','.',',','(',')','[',']','!','?','&','^','%','@','*','$','<','>','|','+','-','{','}','`','~'));
+		for ($i = 0; $i < $len; $i++)
+		{
+			$index = random_int(0, count(CHARS) - 1);
+			@$array .= CHARS[$index];
+		}
+		$f = fopen(dirname($_SERVER['PHP_SELF']) . '/array.dat', 'w+b');
+		fwrite($f, base64_encode($array));
+		fclose($f);
+	
+		$f = fopen(dirname($_SERVER['PHP_SELF']) . '/array.dat', 'rb');
+		$array = base64_decode(fread($f, filesize(dirname($_SERVER['PHP_SELF']) . '/array.dat')));
+		fclose($f);
+		if (strlen($array) != $len)
+		{
+			print_r('Error! Please retry.' . PHP_EOL);
+			return;
 
-	if (file_exists(dirname($_SERVER['PHP_SELF']) . '/denoms.dat'))
-	{
-		$f = fopen(dirname($_SERVER['PHP_SELF']) . '/denoms.dat', 'a+b');
-		$denoms = base64_decode(fread($f, filesize(dirname($_SERVER['PHP_SELF']) . '/denoms.dat')));
-		fclose($f);
-		unset($f);
-		$denoms = json_decode($denoms, true);
-		$max = count($denoms);
-		$denoms[$max + 1]['name'] = $name;
-		unset($name);
-		$denoms[$max + 1]['login'] = $login;
-		unset($login);
-		for ($i = 0; $i < 5; $i++)
-		{
-			$int = random_int(0, 1000);
-			$denoms[$max + 1]['denoms'][$i] = $int;
 		}
-		unset($int);
-		unset($max);
-		$f = fopen(dirname($_SERVER['PHP_SELF']) . '/denoms.dat', 'w+b');
-		fwrite($f, base64_encode(json_encode($denoms, true)));
-		unset($denoms);
-		fclose($f);
-		unset($f);
-		print_r('SUCCESS!');
-	}
-	elseif (!file_exists(dirname($_SERVER['PHP_SELF']) . '/denoms.dat'))
-	{
-		$denoms[1]['name'] = $name;
-		unset($name);
-		$denoms[1]['login'] = $login;
-		unset($login);
-		for ($i = 0; $i < 5; $i++)
+		elseif (strlen($array) == $len)
 		{
-			$denoms[1]['denoms'][$i] = random_int(0, 1000);
+			print_r('Success!' . PHP_EOL);
+			return;
 		}
-		unset($int);
-		$f = fopen(dirname($_SERVER['PHP_SELF']) . '/denoms.dat', 'a+b');
-		fwrite($f, base64_encode(json_encode($denoms, true)));
-		unset($denoms);
-		fclose($f);
-		unset($f);
-		print_r('SUCCESS!');
+		else
+		{
+			print_r('Unkown error!' . PHP_EOL);
+			return;
+		}
 	}
-	exit(0);
 }
-elseif ($argv[1] == 'decrypt')
+function add_record ()
 {
-	unset($argv);
-
-	define('LEN', 4096);
-	define('FACTOR', 3);
+	add_record_start:
+	print_r('Type password length...' . PHP_EOL);
+	$pass_len = readline('>');
+	if (!is_int($pass_len) && ($pass_len < 6 || $pass_len > 32))
+	{
+		print_r('Invalid value. The length must be a number. Not less than 6 and not more than 32' . PHP_EOL);
+		goto add_record_start;
+	}
+	else
+	{
+		print_r('Type service name...' . PHP_EOL);
+		$name = readline('>');
+		print_r('Type login for service...' . PHP_EOL);
+		$login = readline('>');
+		if (file_exists(dirname($_SERVER['PHP_SELF']) . '/denoms.dat'))
+		{
+			$f = fopen(dirname($_SERVER['PHP_SELF']) . '/denoms.dat', 'a+b');
+			$denoms = base64_decode(fread($f, filesize(dirname($_SERVER['PHP_SELF']) . '/denoms.dat')));
+			fclose($f);
+			$denoms = json_decode($denoms, true);
+			$max = count($denoms);
+			$denoms[$max + 1]['name'] = $name;
+			$denoms[$max + 1]['login'] = $login;
+			for ($i = 0; $i < $pass_len; $i++)
+			{
+				$int = random_int(0, 1000);
+				$denoms[$max + 1]['denoms'][$i] = $int;
+			}
+			$f = fopen(dirname($_SERVER['PHP_SELF']) . '/denoms.dat', 'w+b');
+			fwrite($f, base64_encode(json_encode($denoms, true)));
+			fclose($f);
+			print_r('Success!' . PHP_EOL);
+		}
+		elseif (!file_exists(dirname($_SERVER['PHP_SELF']) . '/denoms.dat'))
+		{
+			$denoms[1]['name'] = $name;
+			$denoms[1]['login'] = $login;
+			for ($i = 0; $i < $pass_len; $i++)
+			{
+				$denoms[1]['denoms'][$i] = random_int(0, 1000);
+			}
+			$f = fopen(dirname($_SERVER['PHP_SELF']) . '/denoms.dat', 'a+b');
+			fwrite($f, base64_encode(json_encode($denoms, true)));
+			fclose($f);
+			print_r('Success!' . PHP_EOL);
+		}
+		return;
+	}
+}
+function decrypt ()
+{
+	print_r('Type your pin(more than 0100)...');
+	$factor = readline('>');
 	$f = fopen(dirname($_SERVER['PHP_SELF']) . '/denoms.dat', 'rb');
 	$denoms = base64_decode(fread($f, filesize(dirname($_SERVER['PHP_SELF']) . '/denoms.dat')));
 	$denoms = json_decode($denoms, true);
 	fclose($f);
-	unset($f);
 	$f = fopen(dirname($_SERVER['PHP_SELF']) . '/array.dat', 'rb');
 	$array = base64_decode(fread($f, filesize(dirname($_SERVER['PHP_SELF']) . '/array.dat')));
+	$len = strlen($array);
 	fclose($f);
-	unset($f);
 
-	$max = strlen($array);
-	for ($i = 1; $i < count($denoms) + 1; $i++)
+	for ($i = 0; $i < count($denoms) + 1; $i++)
 	{
 		print_r('----------------------' . PHP_EOL);
-		print_r('Name: ' . $denoms[$i]['name'] . PHP_EOL);
-		print_r('Login: ' . $denoms[$i]['login'] . PHP_EOL);
-		for ($f=0; $f < 10; $f++)
+		print_r('Name: ' . $denoms[$i + 1]['name'] . PHP_EOL);
+		print_r('Login: ' . $denoms[$i + 1]['login'] . PHP_EOL);
+		for ($f = 0; $f < count($denoms[$i + 1]['denoms']) + 1; $f++)
 		{
-			if ($f <= 4){
-				$char[$f] = substr($array, (LEN * FACTOR) / $denoms[$i]['denoms'][$f], 1);
-			}
-			else
-			{
-				$char[$f] = substr($array, (LEN * FACTOR) / ($denoms[$i]['denoms'][$f - 5] * 2), 1);
-			}
+			$char[$f] = substr($array, round(($len * ($factor / 100)) / $denoms[$i + 1]['denoms'][$f]), 1);
 		}
 		unset($f);
-		for ($f=0; $f < 10; $f++)
+		for ($f = 0; $f < count($denoms); $f++)
 		{
 			@$pass .= $char[$f];
 		}
@@ -155,30 +192,6 @@ elseif ($argv[1] == 'decrypt')
 		print_r('----------------------' . PHP_EOL);
 		unset($pass);
 	}
-	unset($pass);
-	unset($max);
-	unset($denoms);
-	unset($array);
-	print_r('SUCCESS!');
-	exit(0);
+	print_r('Success!' . PHP_EOL);
+	return;
 }
-elseif ($argv[1] == 'clear')
-{
-	unlink(dirname($_SERVER['PHP_SELF']) . '/denoms.dat');
-	unlink(dirname($_SERVER['PHP_SELF']) . '/array.dat');
-	unlink($_SERVER['PHP_SELF']);
-	if (!file_exists(dirname($_SERVER['PHP_SELF']) . '/denoms.dat') && !file_exists(dirname($_SERVER['PHP_SELF']) . '/array.dat') && !file_exists($_SERVER['PHP_SELF']))
-	{
-		print_r('SUCCESS!');
-	}
-	else
-	{
-		print_r('ERROR! PLEASE RETRY.');
-	}
-	exit(0);
-}
-else
-{
-	exit(1);
-}
-exit();
